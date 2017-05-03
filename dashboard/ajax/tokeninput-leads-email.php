056cb48c4595af061f7477b0349b0425f8a638a9
Bryann Revina <bryann.revina@gmail.com>
2017-05-03 22:35:11 +0800

    B1 Updates   - Fixed conflict on auto complete on other pages

--- dashboard/ajax/tokeninput-leads-email.php
+++ dashboard/ajax/tokeninput-leads-email.php
@@ -1,16 +1,13 @@
 <?php
-header('Content-Type: application/json');
 require_once("../include/db_connect.php");
 $param  = $_GET['q'];
-$result = $mysqli->query("SELECT LEAD_ID, CLIENT_EMAIL FROM leads WHERE client_email LIKE '%" . $param . "%' LIMIT 10")
-		or die(mysqli_error());
+$data = array();
+$result = $mysqli->query("SELECT LEAD_ID, CLIENT_EMAIL FROM leads WHERE client_email LIKE '%" . $param . "%' LIMIT 10")or die(mysqli_error());
 while($row = mysqli_fetch_array($result)){
-	$arr[] = array('id' => $row['LEAD_ID'], 'name' => $row['CLIENT_EMAIL']);
+	$data[] = array('id' => $row['LEAD_ID'], 'name' => $row['CLIENT_EMAIL']);
+	//echo $row['LEAD_ID'] . ' ' . $row['CLIENT_EMAIL'];
+	//echo '<hr />';
 }
-echo json_encode($arr);
-
-# Optionally: Wrap the response in a callback function for JSONP cross-domain support
-/*if($_GET["callback"]) {
-    $json_response = $_GET["callback"] . "(" . $json_response . ")";
-}*/
+header('Content-Type: application/json');
+echo json_encode($data);
 ?>
