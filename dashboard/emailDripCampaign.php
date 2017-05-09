b41207a303146018d76d73f1fe28b6427391ba4d
Bryan Bio <works.bryan.bio@gmail.com>
2017-05-09 14:47:29 +0800

    Updates for cronjob function

---
+++ dashboard/emailDripCampaign.php
@@ -0,0 +1,109 @@
+<?php
+
+require_once("include/db_connect.php");
+require_once ("include/swiftmailer/lib/swift_required.php");
+$date = date("2017-05-05"); //  	2017-05-05 - Y-m-d
+			$result = $mysqli->query("SELECT * FROM drip_campaign WHERE date_to_send = '". $date ."' ") or die(mysql_error());
+				while($row = mysqli_fetch_array($result)){
+					foreach($row AS $key => $value) {
+						$row[$key] = stripslashes($value);
+					}
+					$email[] = $row;
+				}
+
+		  		
+
+		  if($email){
+		  	foreach ($email as $key => $value):
+		  		echo "<pre>";
+				print_r($value);
+				echo "</pre>";
+
+				$recipient_type = $value['recipient_type'];
+				$recipients	 =  $value['recipients'];
+				$subject     = $value['subject'];	
+				$content     = $value['body_content'];
+
+
+				// This set the email and the name of the recipients from the id of leads 
+				if($recipient_type == 1){ 
+				    $email_list = "'". str_replace(",", "','", $recipients) ."'" ;
+					$email =  array();
+					$result = $mysqli->query("SELECT COMPANY_NAME,CLIENT_EMAIL FROM leads WHERE CLIENT_EMAIL IN(".$email_list.")") or die(mysql_error());
+								while($row = mysqli_fetch_array($result)){
+									foreach($row AS $key => $value) {
+										$row[$key] = stripslashes($value);
+									}
+									$email[] = $row;
+								}
+					if(is_array($email)){
+						
+						foreach ($email as $key => $value) {
+							$email_bcc[$value['CLIENT_EMAIL']] = $value['COMPANY_NAME']; 
+						}
+				    }
+				}
+
+				// This set the email and the name of the recipients from the lead type
+				if($recipient_type == 2){
+
+					$lead_type = $recipients; 
+					$email =  array();
+					$result = $mysqli->query("SELECT COMPANY_NAME,CLIENT_EMAIL FROM leads WHERE LEAD_TYPE = ". '"'.$lead_type.'"' ."  ") or die(mysql_error());
+								while($row = mysqli_fetch_array($result)){
+									foreach($row AS $key => $value) {
+										$row[$key] = stripslashes($value);
+									}
+									$email[] = $row;
+								}
+					if($email){echo "Tae 2";
+						foreach ($email as $key => $value) {
+							if($value['CLIENT_EMAIL'] != ""){
+							   $email_bcc[$value['CLIENT_EMAIL']] = $value['COMPANY_NAME']; 
+							}
+						}
+					}
+				}
+
+
+			// Engine for sending email 
+			// Create the Transport
+			$transport = Swift_SmtpTransport::newInstance('smtp.simplehousesolutionscrm.com', 25)
+			  ->setUsername('info@simplehousesolutionscrm.com')
+			  ->setPassword('abc123!xyz')
+			  ;
+			// Create the Mailer using your created Transport
+			$mailer = Swift_Mailer::newInstance($transport);
+			// Create the message
+			$message = Swift_Message::newInstance($subject)
+
+			// Set the From address with an associative array
+			->setFrom(array('info@simplehousesolutionscrm.com' => 'Info'))
+
+			// Set the To addresses with an associative array
+			->setTo(array('info@simplehousesolutionscrm.com' => 'Info'))
+
+			// Set the BCC for the recipient with multiple email for email blast 
+			->setBcc($email_bcc)
+
+			// Give it a body
+			->setBody($content)
+			;
+
+			// Send the message
+			$result = $mailer->send($message);	
+
+
+			echo "<pre>";
+			print_r($result);
+			print_r($bcc);
+			echo "</pre>";
+          
+		  	endforeach;
+		  } 
+
+
+exit;
+header("Location: thank_you.php");
+
+?>
--- dashboard/submitEmailBlast.php
+++ dashboard/submitEmailBlast.php
@@ -3,9 +3,9 @@
 require_once("include/db_connect.php");
 require_once ("include/swiftmailer/lib/swift_required.php");
 
-if($_POST['search_leads_auto_complete'] != ""){ 
+if($_POST['search_leads_auto_complete'] <> null){ 
 	$email_list = "'". str_replace(",", "','", $_POST['search_leads_auto_complete']) ."'" ;
-
+	$email =  array();
 	$result = $mysqli->query("SELECT COMPANY_NAME,CLIENT_EMAIL FROM leads WHERE LEAD_ID IN(".$email_list.")") or die(mysql_error());
 				while($row = mysqli_fetch_array($result)){
 					foreach($row AS $key => $value) {
@@ -13,18 +13,18 @@ if($_POST['search_leads_auto_complete'] != ""){
 					}
 					$email[] = $row;
 				}
-	foreach ($email as $key => $value) {
-		
-		$bcc[$value['CLIENT_EMAIL']] = $value['COMPANY_NAME']; 
-
-	}
+	if($email <> null){
+		foreach ($email as $key => $value) {
+			$bcc[$value['CLIENT_EMAIL']] = $value['COMPANY_NAME']; 
+		}
+    }
 
 }
 
 if($_POST['lead_type'] != ""){
 
 	echo $lead_type = $_POST['lead_type']; 
-
+	$email =  array();
 	$result = $mysqli->query("SELECT COMPANY_NAME,CLIENT_EMAIL FROM leads WHERE LEAD_TYPE = ". '"'.$lead_type.'"' ."  ") or die(mysql_error());
 				while($row = mysqli_fetch_array($result)){
 					foreach($row AS $key => $value) {
@@ -32,55 +32,58 @@ if($_POST['lead_type'] != ""){
 					}
 					$email[] = $row;
 				}
-
-	foreach ($email as $key => $value) {
-		if($value['CLIENT_EMAIL'] != ""){
-		   $bcc[$value['CLIENT_EMAIL']] = $value['COMPANY_NAME']; 
+	if($email <> null){
+		foreach ($email as $key => $value) {
+			if($value['CLIENT_EMAIL'] != ""){
+			   $bcc[$value['CLIENT_EMAIL']] = $value['COMPANY_NAME']; 
+			}
 		}
 	}
 }
 
 echo "<pre>";
 print_r($bcc);
+print_r($_POST);
 echo "</pre>";
-exit; 
+// exit; 
 
 $subject = $_POST['subject'];
 $content = $_POST['messageMassEmail'];
 
-// // Create the Transport
-// $transport = Swift_SmtpTransport::newInstance('smtp.simplehousesolutionscrm.com', 25)
-//   ->setUsername('info@simplehousesolutionscrm.com')
-//   ->setPassword('abc123')
-//   ;
+// Create the Transport
+$transport = Swift_SmtpTransport::newInstance('smtp.simplehousesolutionscrm.com', 25)
+  ->setUsername('info@simplehousesolutionscrm.com')
+  ->setPassword('abc123!xyz')
+  ;
 
-// // Create the Mailer using your created Transport
-// $mailer = Swift_Mailer::newInstance($transport);
+// Create the Mailer using your created Transport
+$mailer = Swift_Mailer::newInstance($transport);
 
 
-// // Create the message
-// $message = Swift_Message::newInstance()
+// Create the message
+$message = Swift_Message::newInstance($subject)
 
-//   // Give the message a subject
-//   ->setSubject($subject)
+// Set the From address with an associative array
+->setFrom(array('info@simplehousesolutionscrm.com' => 'Info'))
 
-//   // Set the From address with an associative array
-//   ->setFrom(array('info@simplehousesolutionscrm.com' => 'Info'))
+// Set the To addresses with an associative array
+->setTo(array('info@simplehousesolutionscrm.com' => 'Info'))
 
-//   // Set the To addresses with an associative array
-//   ->setTo(array('info@simplehousesolutionscrm.com' => 'Info'))
+// Set the BCC for the recipient with multiple email for email blast 
+->setBcc($bcc)
 
-//   // Set the BCC for the recipient with multiple email for email blast 
-//   ->setBcc($bcc)
+// Give it a body
+->setBody($content)
+;
 
-//   // Give it a body
-//   ->setBody($content)
+// Send the message
+$result = $mailer->send($message);
 
- 
+echo "<pre>";
+print_r($result);
+echo "</pre>";
 
-// // Send the message
-// $result = $mailer->send($message);
-
-// header("Location: thanks.php");
+exit;
+header("Location: thank_you.php");
 
 ?>
